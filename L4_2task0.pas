158f153a039219c29303721d2fcbfe4694e652ff
sedan322 <117731030+sedan322@users.noreply.github.com>
2022-12-15 03:20:19 +0300

    Add files via upload

---
+++ L4_2task0.pas
@@ -0,0 +1,15 @@
+﻿program L4_2task0;
+uses GraphABC;
+begin
+  setpencolor(clblack);
+  moveto(200,200);
+  lineto(300,200);
+  lineto(300,100);
+  lineto(200,100);
+  lineto(200,200);
+  
+  moveto(400,400);
+  lineto(500,400);
+  lineto(450,350);
+  lineto(400,400);
+end.
---
+++ L4_2task1.pas
@@ -0,0 +1,18 @@
+﻿uses graphabc;
+begin
+  setpenwidth(3);
+  SetWindowSize(470,290);
+  Circle(60,200,50);
+  FloodFill(60,200,clred);
+  line(110,200,360,200);
+  line(360,200,235,130);
+  line(235,130,110,200);
+  SetBrushColor(clgreen);
+  FillPolygon((235, 270), (360, 200), (110, 200));
+  Circle(410,200,50);
+  SetBrushColor(clblue);
+  FillPolygon((235, 130), (360, 200), (110, 200));
+  line(110,200,235,270);
+  line(235,270,360,200);
+  floodfill(410,200,clyellow);
+end.
